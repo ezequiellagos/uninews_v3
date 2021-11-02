44b684fbae1f63399f4089e1e4b820ab41b53822
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2021-11-02 18:03:42 -0300

    Add: Configuración básica de funcionalidades del Core

--- uninews/settings.py
+++ uninews/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'api',
 
     'rest_framework',
@@ -120,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

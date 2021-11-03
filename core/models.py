ec7edafcc5ec42ff752b3ce28b066308d6378c09
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2021-11-03 19:08:27 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SITE_CONFIG(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, default="")
+    def __str__(self):
+        return self.name
+

0c9e31a1cdff330447e47991834aa318a07efdc9
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2021-11-23 16:48:38 -0300

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
+import os
+
 from django.shortcuts import render
 from .decorators import group_required
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-supported_groups = ['user', 'admin']
+# Load .env file
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+supported_groups = os.environ.get("SUPPORTED_GROUPS").split(',')
 
 @group_required(*supported_groups)
 def home(request):
